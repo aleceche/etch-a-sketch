13da06b1c1c4706a57f0261e8386be299ca0ceea
Alec Echevarria <alecech1994@gmail.com>
2021-02-08 16:42:04 -0800

    Added a reset button

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const container = document.querySelector('.container');
+const clearButton = document.createElement('button');
+clearButton.classList.add('clearButton');
+clearButton.textContent = 'Reset Board';
+container.parentNode.insertBefore(clearButton, container.parentNode.childNodes[0]);
 
 function setDefaultBoard() {
     for (let i = 0; i < 16; i++) {
